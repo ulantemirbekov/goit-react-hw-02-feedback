6ae515e5ade90ab025b938551cc06ed9848547da
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-06 23:03:12 +0300

    Section added

---
+++ src/Components/Section/Section.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+const Section = ({ title, children }) => (
+    <section>
+        <h2>{title}</h2>
+        {children}
+    </section>
+);
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+export default Section;
